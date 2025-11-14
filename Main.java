4adf34e8b837c1984e189d8115c99016fbebf76d
aayush8851 <162785187+aayush8851@users.noreply.github.com>
2025-11-14 10:19:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+
+//int y=sc.nextInt();
+//for (int x=1;x<=10;x++){
+//    System.out.println(y+"*"+x+ "=" + y*x + " ");
+//
+//
+//}
+        int t= sc.nextInt();
+        for (int r=10;r>=1;r=r-1){
+            System.out.println(t+"*"+r+"="+t*r+" ");
+        }
+    }
+}
