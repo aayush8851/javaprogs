a41e0ec0d81c4b694e6e72d18f63f3af9af77410
aayush8851 <162785187+aayush8851@users.noreply.github.com>
2025-11-14 10:50:31 +0530

    Create newproject1.java

---
+++ newproject1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class newproject1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for (int y=1;y<=n;y++){
+            System.out.print(y + " ");
+        }
+    }
+}
